a6a6404d8422e8544823c319d8b132c2043ba41a
lucasbarroscode <lucasbabezerra@gmail.com>
2022-11-09 12:40:13 -0300

    commit initial

---
+++ src/test/java/br/com/lucasbarroscode/aws_project/AwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasbarroscode.aws_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
